2b867644b0d6a1936c56ef1dd01f04725068edfe
Clément Corbin <43074339+corbin-c@users.noreply.github.com>
2023-07-04 16:25:38 +0200

    polyfill fetch on server

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const fs = require("fs");
 ESrequire = (path) => {
   path = fs.readFileSync(path,"utf8");
+  if (typeof window === "undefined") {
+    path = "const fetch = require('node-fetch');\n"+path
+  }
   path = path.replace("\nexport {","\nmodule.exports = {");
   return eval(path);
 }
